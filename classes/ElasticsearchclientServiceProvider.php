62dc09f50e3cd2e0bf1c2c820334d1b0cc820877
Shoxabbos Olimjonov <19941001@inbox.ru>
2020-05-20 10:59:42 +0500

    Bug fix in settings

--- classes/ElasticsearchclientServiceProvider.php
+++ classes/ElasticsearchclientServiceProvider.php
@@ -3,17 +3,30 @@
 use Elasticsearch\Client;
 use Elasticsearch\ClientBuilder;
 use October\Rain\Support\ServiceProvider;
+use Shohabbos\Elasticsearch\Models\Settings;
 
 class ElasticsearchclientServiceProvider extends ServiceProvider
 {
 
     public function register()
     {
-    	$this->app->bind(Client::class, function ($app) {
-            return ClientBuilder::create()
-                ->setHosts(["localhost:9200"])
-                ->build();
-        });
+
+    	\App::before(function($request) {
+    		$data = Settings::get('fields', []);	
+
+    		$hosts = [];
+    		foreach ($data as $key => $value) {
+    			$hosts[] = implode(":", $value);
+    		}
+
+    		$this->app->bind(Client::class, function ($app) use ($hosts) {
+	            return ClientBuilder::create()
+	                ->setHosts($hosts)
+	                ->build();
+	        });
+    	});
+    	
+    	
     }
 
 }
